7bf248c9c2dcd02722ab2308c819acebf854e0c8
Helen Zhou <helen.zhou@thoughtworks.com>
2020-04-14 14:01:12 +0100

    Adds frontend

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const socketIO = require('socket.io');
+const express = require("express");
+const socketIO = require("socket.io");
+const path = require("path");
 
-const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+const PORT = process.env.PORT || 5000;
+const INDEX = "/client/build/index.html";
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+const app = express();
+
+const server = app
+  .use(express.static(path.join(__dirname, "client", "build")))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 const io = socketIO(server);
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+io.on("connection", (socket) => {
+  console.log("Client connected");
+  socket.on("disconnect", () => console.log("Client disconnected"));
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+setInterval(() => io.emit("time", new Date().toTimeString()), 1000);
